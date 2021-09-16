7861fe05161ad134b5eb1cb37394f4babba4ad42
GitHub Action <action@github.com>
2021-09-16 14:59:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/davblain/keemuntemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/davblain/keemuntemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.davblain.keemuntemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.davblain.keemuntemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/davblain/keemuntemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/davblain/keemuntemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.davblain.keemuntemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.davblain.keemuntemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
